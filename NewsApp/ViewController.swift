fda1490151e0ba02fcca3be939a458d27638b2a4
HarshaJamedar <harsha_jemedar@csu.fullerton.edu>
2024-02-29 19:50:07 -0800

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by HARSHAVARDHAN JEMEDAR on 2/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
